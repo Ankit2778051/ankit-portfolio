68458797cd9b79b01b0b9852b0ed77728603dc48
AnkitraiHitech <163105012+AnkitraiHitech@users.noreply.github.com>
2026-07-07 21:20:20 +0530

    portfolio

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -17,7 +17,7 @@ function Footer() {
           <div className="flex items-center gap-5">
             <Link
               target="_blank"
-              href="https://github.com/said7388/developer-portfolio"
+              href="https://github.com/Ankit2778051/ankit-portfolio"
               className="flex items-center gap-2 uppercase hover:text-[#16f2b3]"
             >
               <IoStar />
@@ -25,7 +25,7 @@ function Footer() {
             </Link>
             <Link
               target="_blank"
-              href="https://github.com/said7388/developer-portfolio/fork"
+              href="https://github.com/Ankit2778051/ankit-portfolio/fork"
               className="flex items-center gap-2 uppercase hover:text-[#16f2b3]"
             >
               <CgGitFork />
